3672d636a6d1d68e0c6f2841f321e253e5ea0258
Abem Woldesenbet <abemkibatu101@gmail.com>
2025-12-03 11:06:37 -0600

    refactor: Move AgentStreamingCallback to demo-specific callbacks for better separation of concerns

--- exaid_core/callbacks/__init__.py
+++ exaid_core/callbacks/__init__.py
@@ -1,5 +1,5 @@
 """Callbacks Module - LangChain callback handlers for streaming"""
 
-from exaid_core.callbacks.agent_streaming_callback import AgentStreamingCallback
-
-__all__ = ['AgentStreamingCallback']
+# Callbacks for EXAID core functionality
+# Note: AgentStreamingCallback has been moved to demos/cdss_example/callbacks/
+# for better separation of core vs demo-specific code
